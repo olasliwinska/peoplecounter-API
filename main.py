1743a7fdb42e7c8d905e9de114e97e87e076bd87
Aleksandra Śliwińska <olasliwinnska1@gmail.com>
2024-01-28 12:32:23 +0100

    add people counter

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import cv2
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('rynek-Wilkowyje.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(4, 4))
+
+        return {'count': len(boxes)}
+
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
+api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
