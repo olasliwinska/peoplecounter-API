31592e1cbdc5f7164c8f2315f4abf73cc996f487
Aleksandra Śliwińska <olasliwinnska1@gmail.com>
2024-01-28 16:00:55 +0100

    add url counter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -17,11 +17,23 @@ class PeopleCounter(Resource):
         return {'count': len(boxes)}
 
 
+class PeopleCounterUrl(Resource):
+    def get(self):
+        url = request.args.get('url')
+        if url:
+            img2 = cv2.imread(url)
+            boxes, weights = hog.detectMultiScale(img2, winStride=(4, 4))
+            return {'count url': len(boxes)}
+        else:
+            return {'error'}
+
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 
+api.add_resource(PeopleCounterUrl, '/link')
 api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
