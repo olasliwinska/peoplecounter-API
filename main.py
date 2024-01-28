8fe683decd1b9182ab54ef6a60337fea92be240b
Aleksandra Śliwińska <olasliwinnska1@gmail.com>
2024-01-28 15:26:12 +0100

    add counter from url

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from urllib.parse import urlparse
+
 import cv2
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -17,11 +19,26 @@ class PeopleCounter(Resource):
         return {'count': len(boxes)}
 
 
+class PeopleCounterURL(Resource):
+    def get(self):
+        url = request.args.get('url')
+
+        if url and urlparse(url).scheme and urlparse(url).netloc:
+            img = cv2.imread(url)
+            if img is not None:
+                boxes, weights = hog.detectMultiScale(img, winStride=(4, 4))
+
+                return {'count': len(boxes)}
+
+        return {'error': 'Podany URL nie jest poprawny lub brak parametru "url" w zapytaniu.'}
+
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 
+api.add_resource(PeopleCounterURL, '/licz')
 api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
